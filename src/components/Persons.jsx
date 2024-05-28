e46b95c89d50427891ac5a049c100c493b0e8bca
Moi~Rouhs <carlos@z3.com>
2024-05-27 20:56:41 -0500

    create editing mode

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -1,49 +1,85 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
 import { Person } from './Person';
+import { useState } from 'react';
+export const Persons = ({ persons, setPersons }) => {
 
-export const Persons = () => {
+    const [editingId, setEditingID] = useState(null);
+    const [editedPerson, setEditedPerson ] = useState({ name:'', role:'', img:''});
+    const [isEditing, setIsEditing ] = useState(false);
+    
+    const handleChange = (e)=>{
+        const {name, value } = e.target;
+        setEditedPerson(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+    const handleEdit = (id)=>{
+        setEditingID(id);
+        setIsEditing(true);
+        const personToEdit = persons.find(person => person.id === id);
+        setEditedPerson({...personToEdit});
+    };
 
-  const [ persons, setPersons ] = useState([
-    {
-      id: 1,
-      name: "Oscar Eduardo",
-      role: "Frontend Developer",
-      img: "https://bootdey.com/img/Content/avatar/avatar6.png",
-    },
-    {
-      id: 2,
-      name: "Carolina",
-      role: "Backend Developer",
-      img: "https://bootdey.com/img/Content/avatar/avatar3.png",
-    },
-    {
-      id: 3,
-      name: "Catalina",
-      role: "UI/UX Designer",
-      img: "https://bootdey.com/img/Content/avatar/avatar8.png",
-    },
-  ]);
-
-  return (
-    <div>
-      <h2>IT Team</h2>
-      <div className='container d-flex justify-content-center '>
-        <div className='d-flex flex-row'>
-          {persons.map((person) => {
-            return (
-              <div>
-                <Person
-                  key={person.id}
-                  name={person.name}
-                  role={person.role}
-                  img={person.img}
-                />
+    const handleSave = (e)=>{
+        setPersons(persons.map(person => person.id === editingId ? editedPerson: person ));
+        setEditingID(null);
+        setEditedPerson({ name:'', role:'', img:''});
+        setIsEditing(false);
+    };
+    
+    return (
+        <div>
+          <h2>IT Team</h2>
+          <div className='container d-flex justify-content-center '>
+            <div className='d-flex flex-row'>
+              {persons.map((person) => {
+                  return (
+                      <div>
+                        <Person
+                          key={person.id}
+                          name={person.name}
+                          role={person.role}
+                          img={person.img}
+                          handleEdit={()=>handleEdit(person.id)}
+                        />
+                      </div>
+                  );
+              })}
+            </div>
+          </div>
+          <div className='mt-4'>
+            <h2>Modificar Datos</h2>
+            <div className='d-flex flex-column mt-4'>
+              <input
+                name="name"
+                type="text"
+                placeholder="Nombre"
+                value={editedPerson.name}
+                onChange={handleChange}
+                className='form-control mb-2'
+              />
+              <input
+                name="role"
+                type="text"
+                placeholder="Role"
+                value={editedPerson.role}
+                onChange={handleChange}
+                className='form-control mb-2'
+              />
+              <input
+                name="img"
+                type="text"
+                placeholder="Url de la imagen"
+                value={editedPerson.img}
+                onChange={handleChange}
+                className='form-control mb-2'
+              />
+              <div className="mt-2">
+                <button className='btn btn-primary' onClick={handleSave}>Guardar</button>
               </div>
-            )
-          })}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  )
+    );
 }
